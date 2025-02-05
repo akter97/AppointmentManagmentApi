c215b7d91cf690790024682f670bb9df35cb309b
Akter Hossain <akterhossenraj2040@gmail.com>
2025-02-06 00:34:06 +0600

    enum set of api

--- Models/User.cs
+++ Models/User.cs
@@ -12,8 +12,19 @@ namespace AppointmentManagmentApi.Models
         [Required(ErrorMessage = "Username Contact is required.")]
         public string Username { get; set; } = null;
 
+        
+        public UserRole Role { get; set; }
+
         [Required(ErrorMessage = "Password is required.")]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         public string Password { get; set; } = null;
+    }
+
+
+    public enum UserRole
+    {
+        Admin = 1,
+        User = 2,
+        Guest = 3
     }
 }
